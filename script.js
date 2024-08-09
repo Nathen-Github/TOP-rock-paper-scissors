c31a8c33235a69f91e9f58ebfbbee5f6e667d987
Nathen Antony <nathenantony@icloud.com>
2024-08-09 18:01:14 +0400

    Added comments to explain code

--- script.js
+++ script.js
@@ -1,35 +1,47 @@
+//The starting function
 playGame();
 
 function playGame() {
+    //5 rounds are played
     for (i = 0; i < 5; i++) {
+        //playRound is the function to play one round of RPS
         let result = playRound(humanSelection(), computerSelection());
-        console.log(result);
     }
 }
 
 function playRound(humanSelection, computerSelection) {
+    //Game win and lose logic
     if (humanSelection === "rock") {
         if (computerSelection === "rock") {
+            console.log("It's a tie!")
             return "tie";
         } else if (computerSelection === "paper") {
+            console.log("You lose! Paper beats Rock!")
             return "computer";
         } else if (computerSelection === "scissors") {
+            console.log("You win! Rock beats Scissors!")
             return "player"
         }
     } else if (humanSelection === "paper") {
         if (computerSelection === "rock") {
+            console.log("You win! Paper beats Rock!")
             return "player";
         } else if (computerSelection === "paper") {
+            console.log("It's a tie!")
             return "tie";
         } else if (computerSelection === "scissors") {
+            console.log("You lose! Scissors beats Paper!")
             return "computer"
         }
     } else if (humanSelection === "scissors") {
         if (computerSelection === "rock") {
+            console.log("You lose! Rock beats Scissors!")
             return "computer";
         } else if (computerSelection === "paper") {
+            console.log("You win! Scissors beats Paper!")
             return "player";
         } else if (computerSelection === "scissors") {
+            console.log("It's a tie!")
             return "tie"
         }
     }
@@ -37,9 +49,11 @@ function playRound(humanSelection, computerSelection) {
 
 function humanSelection() {
     let selection;
+    //Loops until input is valid
     while (true) {
         selection = prompt("Rock, paper, or scissors? ").toLowerCase();
         if (selection == "rock" || selection == "paper" || selection == "scissors") {
+            //Breaks the loop once input is valid
             break;
         }
     }
@@ -47,7 +61,10 @@ function humanSelection() {
 }
 
 function computerSelection() {
+    //Math.random() * (max - min) + min -> this is the code to get a random number between max-min.
+    //We convert the float result to an int by converting to an array and getting the first index
     let random = (Math.random() * (3 - 1) + 1).toString().split('')[0];
+    //Unary plus to convert String result to Number
     switch(+random) {
         case 1:
             return "rock";
